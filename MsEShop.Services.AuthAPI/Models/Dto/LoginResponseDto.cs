78348fefe38220da3c7f03f196bdabc465d59d4d
WKE\nuria.cambronero <nuria.cambronero@external.wolterskluwer.com>
2026-03-02 11:23:37 +0100

    Add Login and Register endpoints and DTOs.

---
+++ MsEShop.Services.AuthAPI/Models/Dto/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MsEShop.Services.AuthAPI.Models.Dto
+{
+    public class LoginResponseDto
+    {
+        public UserDto User { get; set; }
+        public string Token { get; set; }
+    }
+}
