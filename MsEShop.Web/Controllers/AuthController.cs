7d84a1607ad72ac2297e051f3c4cce178aecb8a5
WKE\nuria.cambronero <nuria.cambronero@external.wolterskluwer.com>
2026-03-04 11:50:24 +0100

    Add Login and Register UI

---
+++ MsEShop.Web/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MsEShop.Web.Models;
+using MsEShop.Web.Service.IService;
+
+namespace MsEShop.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+    }
+}
