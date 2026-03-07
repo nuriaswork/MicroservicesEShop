e7b9914cc392cceb4e57e078e6edf9a43ceb32d5
WKE\nuria.cambronero <nuria.cambronero@external.wolterskluwer.com>
2026-03-07 17:20:50 +0100

    Add ProductAPI

---
+++ MsEShop.Services.ProductAPI/Constants/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace MsEShop.Services.ProductAPI.Constants
+{
+    public static class Roles
+    {
+        public const string Admin = "ADMIN";
+        public const string Customer = "CUSTOMER";
+    }
+}
---
+++ MsEShop.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,136 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MsEShop.Services.ProductAPI.Data;
+using MsEShop.Services.ProductAPI.Models;
+using MsEShop.Services.ProductAPI.Models.Dto;
+
+namespace MsEShop.Services.ProductAPI.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    [Authorize]
+    public class ProductAPIController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+        private ResponseDto _responseDto;
+
+        public ProductAPIController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+            _responseDto = new ResponseDto() { Success = true };
+        }
+
+        // GET: api/ProductAPI
+        [HttpGet]
+        public ResponseDto GetProducts()
+        {
+            try
+            {
+                IEnumerable<Product> products = _context.Products.ToList();
+                _responseDto.Result = _mapper.Map<IEnumerable<ProductDto>>(products);
+            }
+            catch (Exception e)
+            {
+                _responseDto.Result = false;
+                _responseDto.Message = e.Message;
+            }
+            return _responseDto;
+        }
+
+        // GET: api/ProductAPI/5
+        [HttpGet("{id}")]
+        public ResponseDto GetProduct(int id)
+        {
+            try
+            {
+                Product product = _context.Products.First(p => p.ProductId == id);
+                _responseDto.Result = _mapper.Map<ProductDto>(product);
+            }
+            catch (Exception e)
+            {
+                _responseDto.Success = false;
+                _responseDto.Message = e.Message;
+            }
+            return _responseDto;
+        }
+
+        [HttpGet]
+        [Route("GetByName/{name}")]
+        public ResponseDto GetByCode(string name)
+        {
+            try
+            {
+                Product product = _context.Products.First(p => p.Name == name);
+                _responseDto.Result = _mapper.Map<ProductDto>(product);
+            }
+            catch (Exception e)
+            {
+                _responseDto.Success = false;
+                _responseDto.Message = e.Message;
+            }
+            return _responseDto;
+        }
+
+        // PUT: api/ProductAPI/5
+        [HttpPut]
+        [Authorize(Roles = Constants.Roles.Admin)]
+        public ResponseDto PutProduct([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                Product product = _mapper.Map<Product>(productDto);
+                _context.Products.Update(product);
+                _context.SaveChanges();
+                _responseDto.Result = _mapper.Map<ProductDto>(product);
+            }
+            catch (Exception e)
+            {
+                _responseDto.Success = false;
+                _responseDto.Message = e.Message;
+            }
+            return _responseDto;
+        }
+
+        // POST: api/ProductAPI
+        [HttpPost]
+        [Authorize(Roles = Constants.Roles.Admin)]
+        public ResponseDto PostProduct([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                Product product = _mapper.Map<Product>(productDto);
+                _context.Products.Add(product);
+                _context.SaveChanges();
+                _responseDto.Result = _mapper.Map<ProductDto>(product);
+            }
+            catch (Exception e)
+            {
+                _responseDto.Success = false;
+                _responseDto.Message = e.Message;
+            }
+            return _responseDto;
+        }
+
+        // DELETE: api/ProductAPI/5
+        [HttpDelete("{id:int}")]
+        [Authorize(Roles = Constants.Roles.Admin)]
+        public ResponseDto DeleteProduct(int id)
+        {
+            try
+            {
+                Product product = _context.Products.First(p => p.ProductId == id);
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                _responseDto.Success = false;
+                _responseDto.Message = e.Message;
+            }
+            return _responseDto;
+        }
+    }
+}
