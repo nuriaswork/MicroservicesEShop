4be8210b29f08f92ebcfa19714b1e112f89d4607
WKE\nuria.cambronero <nuria.cambronero@external.wolterskluwer.com>
2026-03-08 17:42:41 +0100

    Home page and product details.

--- MsEShop.Web/Controllers/HomeController.cs
+++ MsEShop.Web/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MsEShop.Web.Models;
+using MsEShop.Web.Service.IService;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace MsEShop.Web.Controllers
@@ -7,15 +10,46 @@ namespace MsEShop.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductService productService)
         {
             _logger = logger;
+            _productService = productService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<ProductDto> list = [];
+
+            ResponseDto response = await _productService.GetAllProductsAsync();
+
+            if (response != null && response.Success)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response.Message;
+            }
+            return View(list);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> ProductDetails(int productId)
+        {
+            ResponseDto response = await _productService.GetProductByIdAsync(productId);
+            ProductDto model = new();
+
+            if (response != null && response.Success)
+            {
+                model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response.Message;
+            }
+            return View(model);
         }
 
         public IActionResult Privacy()
--- MsEShop.Web/Models/ProductDto.cs
+++ MsEShop.Web/Models/ProductDto.cs
@@ -1,12 +1,17 @@
-﻿namespace MsEShop.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MsEShop.Web.Models
 {
     public class ProductDto
     {
         public int ProductId { get; set; }
-        public required string Name { get; set; }
+        public string Name { get; set; }
         public double Price { get; set; }
         public string Description { get; set; }
         public string CategoryName { get; set; }
         public string ImageUrl { get; set; }
+
+        [Range(1,100)]
+        public int Count { get; set; } = 1;
     }
 }
